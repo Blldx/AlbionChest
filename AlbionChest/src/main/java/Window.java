ffe5456a069d0b6959919c787161913c1b745742
Bldx <kamil.zdrojew@gmail.com>
2020-03-18 18:57:59 +0100

    Hehe

---
+++ AlbionChest/src/main/java/Window.java
@@ -0,0 +1,20 @@
+import com.sun.tools.javac.comp.Flow;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.*;
+
+
+public class Window extends JFrame {
+    public Window(){
+        super("To dla Ciebie Michałku <3");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(700, 500);
+        setLocation(50, 50);
+        setLayout(new FlowLayout());
+
+        add(new Button("Suma"));
+
+    }
+}
